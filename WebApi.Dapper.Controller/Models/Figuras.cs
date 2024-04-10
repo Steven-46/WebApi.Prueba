323335a98affe5be1844dca50c432c322344f2ba
Steven-46 <margueis_46@yahoo.com>
2024-04-10 16:41:34 -0600

    Agregando Modelo para figuras de accion

---
+++ WebApi.Dapper.Controller/Models/Figuras.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Dapper.Controller.Models
+{
+    public class Figuras
+    {
+        public int Id { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public decimal precio { get; set; }
+    }
+}
--- WebApi.Dapper.Controller/obj/Debug/net8.0/WebApi.Dapper.Controller.AssemblyInfo.cs
+++ WebApi.Dapper.Controller/obj/Debug/net8.0/WebApi.Dapper.Controller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi.Dapper.Controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0a7ae8260300dbeb9dcb34343249e7949643bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi.Dapper.Controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi.Dapper.Controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
